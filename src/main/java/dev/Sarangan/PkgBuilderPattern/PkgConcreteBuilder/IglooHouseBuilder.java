53529c0504d1e827a176733064b55b3a70e442c2
Sarangan Baskar <saranganb1993@gmail.com>
2023-10-21 17:40:36 +0530

    Design Patterns using JAVA code base-Builder Pattern

---
+++ src/main/java/dev/Sarangan/PkgBuilderPattern/PkgConcreteBuilder/IglooHouseBuilder.java
@@ -0,0 +1,38 @@
+package dev.Sarangan.PkgBuilderPattern.PkgConcreteBuilder;
+
+import dev.Sarangan.PkgBuilderPattern.PkgBuilder.HouseBuilder;
+import dev.Sarangan.PkgBuilderPattern.PkgProducts.House;
+
+public class IglooHouseBuilder implements HouseBuilder {
+
+    private final House house;
+
+    public IglooHouseBuilder() {
+        this.house = new House();
+    }
+
+    @Override
+    public void buildBasement() {
+        house.setBasement("Ice Bars for Igloo basement");
+    }
+
+    @Override
+    public void buildStructure() {
+        house.setStructure("Ice blocks as Igloo structure");
+    }
+
+    @Override
+    public void buildRoof() {
+        house.setRoof("Ice Dome for Igloo");
+    }
+
+    @Override
+    public void buildInterior() {
+        house.setInterior("Ice Carvings for Igloo");
+    }
+
+    @Override
+    public House getHouse() {
+        return this.house;
+    }
+}
